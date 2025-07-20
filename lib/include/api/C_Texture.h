711decd9b36a1e0461d8674813feccd16f0894ce
Atom Roese <40679658+ARoese@users.noreply.github.com>
2025-07-20 17:43:49 -0400

    expose access to texture data in C api

--- lib/include/api/C_Texture.h
+++ lib/include/api/C_Texture.h
@@ -5,6 +5,7 @@
 #ifndef C_TEXTURE_H
 #define C_TEXTURE_H
 #include "compat_wrappers.h"
+#include "D_VectorTypes.h"
 
 #ifdef __cplusplus
     extern "C" {
@@ -13,7 +14,10 @@
         typedef void* COMPAT_PTRTYPE(Texture);
         //C_Texture_Ptr make_Texture(int x, int y);
 
-        void destroy_Texture(C_Texture_Ptr toDestroy);
+        void destroy_Texture(COMPAT_PTRTYPE(Texture) toDestroy);
+        // this pointer is non-owned
+        d_uchar3* Texture_data(COMPAT_PTRTYPE(Texture) texture);
+        d_uint2 Texture_dimensions(COMPAT_PTRTYPE(Texture) texture);
 
         void save_to_file( char* outFile, COMPAT_PTRTYPE(Texture) texture );
 
